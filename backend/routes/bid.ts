8664827d5d8f4f96ba81997f976d55960fcd6c32
Yannis1112 <yannis.philippot@gmail.com>
2024-03-29 15:33:32 +0100

    route bids avec productId

--- backend/routes/bid.ts
+++ backend/routes/bid.ts
@@ -9,11 +9,18 @@ router.delete('/api/bids/:bidId', async (req, res) => {
   res.status(600).send()
 })
 
-router.post('/api/products/:productId/bids', async (req, res) => {
+router.post('/api/products/:productId/bids', authMiddleware, async (req, res) => {
+  if(!req.body["price"]){
+    res.status(400).json(
+    {
+      "error": "Invalid or missing fields",
+	    "details": ["price"]
+    })
+  }
+
   const product = await Product.findOne({
     where: { id: req.params["productId"]}
   })
-
 
   if(product){
     const newBid = await Bid.create({
@@ -22,9 +29,15 @@ router.post('/api/products/:productId/bids', async (req, res) => {
       "date": product.endDate,
       "bidderId":product.bids
     })
-    res.status(201).json({"created": "..."})
+    res.status(201).json({
+      "id": newBid.id,
+      "productId": newBid.productId,
+      "price": newBid.price,
+      "date": newBid.date,
+      "bidderId":newBid.bidderId
+    })
   }else{
-    res.status(404).json({"error": "..."})
+    res.status(404).json({"error": "Product not found"})
   }
 })
 
