aa4e72c2169ebca46db71271aeb20d85615a6887
p2204846 <clement.pannecoucke@etu.univ-lyon1.fr>
2024-04-05 16:11:18 +0200

    1ers tests

--- backend/routes/product.ts
+++ backend/routes/product.ts
@@ -2,6 +2,8 @@ import express from 'express'
 import { Product, Bid, User } from '../orm/index.js'
 import authMiddleware from '../middlewares/auth.js'
 import { getDetails } from '../validators/index.js'
+import { Request } from 'express';
+import { Token } from 'types/types.js';
 
 const router = express.Router()
 
@@ -21,8 +23,14 @@ router.get('/api/products', async (req, res) => {
   }
 });
 
-router.post('/api/products',(req, res) => {
-  const { name, description, category, originalPrice, pictureUrl, endDate, sellerId } = req.body;
+router.post('/api/products', authMiddleware, async (req: Request<Record<string,string>, any, any> & {user?: Token}, res) => {
+  const { name, description, category, originalPrice, pictureUrl, endDate} = req.body;
+  const sellerId = req.user?.id;
+
+  if (!name ||!description ||!category ||!originalPrice ||!pictureUrl ||!endDate ||!sellerId) {
+    return res.status(400).send({"error" : "Invalid or missing fields", "details" : ""});
+    req.body.details;
+  }
 
   const newProduct = {
     name,
@@ -35,21 +43,30 @@ router.post('/api/products',(req, res) => {
   };
 
   Product.create(newProduct)
-    .then((product) => {
+ .then((product) => {
       res.status(201).json(product);
     })
-    .catch((error) => {
+ .catch((error) => {
       res.status(401).send(error);
     });
 });
 
 
 // You can use the authMiddleware with req.user.id to authenticate your endpoint ;)
+/*
+router.get('/api/products/:productId', (req, res) => {
+  const productId = req.params.productId;
+  const product = Product.findOne({where : {id: req.params["productId"]}})
 
-router.post('/api/products', (req, res) => {
-  res.status(600).send()
-})
-
+  .then((product) => {
+    if(!product === null){res.status(200).json(product);}
+    
+  })
+.catch((error) => {
+    res.status(404).send(error);
+  });
+});
+*/
 router.put('/api/products/:productId', async (req, res) => {
   res.status(600).send()
 })
